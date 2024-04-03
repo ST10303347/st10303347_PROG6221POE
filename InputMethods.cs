8e18df50b946ec66534db3643f2e7785e686b49e
ZAYYYY <st10303347@vcconnect.edu.za>
2024-04-03 23:05:48 +0200

    Updated menu, Created another number validation class, Started on the "create Recipe" option, User can now enter ingredients and quantities and they are stored in an array, only quantity is validated.

--- InputMethods.cs
+++ InputMethods.cs
@@ -12,39 +12,41 @@ namespace st10303347_PROG6221POE
         {
         }
 
-        public static int numberValidation(int min, int max) { 
-        int finalChoice = 0;
-        bool correctInput = false;
-        string userInput;
+        public static int numberValidation(int min, int max)
+        {
+            int finalChoice = 0;
+            bool correctInput = false;
+            string userInput;
             int intUserInput;
 
 
 
-            while (!correctInput) {
+            while (!correctInput)
+            {
 
-                Console.Write("Enter a number between " + min + " and " + max + "\n");
-                 userInput = Console.ReadLine();
-              
-                    try
+               
+                userInput = Console.ReadLine();
+
+                try
+                {
+
+                    intUserInput = int.Parse(userInput);
+
+                    if (intUserInput >= min && intUserInput <= max)
                     {
-
-                        intUserInput = int.Parse(userInput);
-
-                        if (intUserInput >= min && intUserInput <= max)
-                        {
-                            finalChoice = intUserInput;
-                            correctInput = true;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Please enter a number between" + min + " and " + max);
-                        }
-
-
+                        finalChoice = intUserInput;
+                        correctInput = true;
                     }
-                    catch (FormatException)
+                    else
                     {
                         Console.WriteLine("Please enter a number between" + min + " and " + max);
+                    }
+
+
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Please enter a number between " + min + " and " + max);
 
                 }
 
@@ -62,5 +64,37 @@ namespace st10303347_PROG6221POE
 
             return finalChoice;
         }
+
+        public static int CheckQuantity(int min, int max)
+        {
+            int finalChoice = 0;
+            bool correctInput = false;
+            string userInput;
+            int intUserInput;
+
+            while (!correctInput)
+            {
+
+                Console.Write("Quantity: ");
+                userInput = Console.ReadLine();
+
+
+                try
+                {
+                    intUserInput = int.Parse(userInput);
+                    
+                    if (intUserInput >= min && intUserInput <= max)
+                    {
+                        finalChoice = intUserInput;
+                        correctInput = true;
+                    }
+                }
+                catch (FormatException){ Console.WriteLine("Enter a number!"); }
+            
+            
+            }
+
+                return finalChoice;
+            }
+        }
     }
-}
--- Program.cs
+++ Program.cs
@@ -4,26 +4,78 @@ namespace st10303347_PROG6221POE
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
-            //InputMethods m = new InputMethods();
+        //Declarations   
             Boolean screenON = true;
+           
+
+
+
+
+        //Starts Here
             Console.WriteLine("Welcome to Recipe.net8");
+
+
+        //Boolean to keep screen on, Application will always return to the begining of a while loop after breaking out of a if statement or switch case
             while (screenON) {
-                Console.WriteLine("1. Create a new recipe\n2.View Recipe\n3.Exit Application");
-                int menuchoice = InputMethods.numberValidation(1, 3);
-               
+
+                Console.WriteLine("1. Create a new recipe\n2.View Recipe\n3.Scale recipe\n4.Reset Quantities\n5.Clear Data\n6.Exit Aplication");
+                int menuchoice = InputMethods.numberValidation(1, 6);
+            //Above is a method to validate user input so application does not crash  
                 switch (menuchoice)
                 {
                     case 1:
-                        Console.WriteLine("you picked 1");
+                        Console.WriteLine("How many ingredients will you be entering?");
+                        int noOfIngredients = InputMethods.numberValidation(1, 100);
+                        String[] ingredientsArr = new String[noOfIngredients];
+                        int[] QuantitiesArr = new int[noOfIngredients];
+                        Console.Clear();
+
+                        for (int i = 0; i < noOfIngredients; i++)
+                        {
+
+                            int n = 1 + i;
+                            Console.Write("Ingredient "+ n +"\nName: ");
+                            String ingName = Console.ReadLine();
+                            Console.Write("Measurement: ");
+                            String ingMeasurement = Console.ReadLine();
+                            int quantity = InputMethods.CheckQuantity(1, 1000);
+                            ingredientsArr[i] = ingName;
+                            QuantitiesArr[i] = quantity;    
+                            Console.Clear();
+
+                        }   
+
+                        Console.WriteLine("How many Steps will the recipe have?");    
+                        int noOfSteps = InputMethods.numberValidation(1, 100);
+                        String[] stepsArr = new string[noOfSteps];
+
+                        
+
+
                         break;
+
+
                     case 2:
-                        Console.WriteLine("you picked 2");
+
+                         
                         break;
                     case 3:
-                        Console.WriteLine("BYEEEE");
-                        System.Environment.Exit(0); 
+                        Console.WriteLine("3333");
+                        
+                        break;
+
+                    case 4:
+                        Console.WriteLine("you picked 4");
+                        break;
+                    case 5:
+                        Console.WriteLine("you picked 5");
+                        break;
+                    case 6:
+                        Console.WriteLine("Have an amazing day ;)");
+                        System.Environment.Exit(0);
                         break;
 
 
