4bca00914ac8a9ceb7d83a02beb1d40ed1099478
ZAYYYY <st10303347@vcconnect.edu.za>
2024-04-03 15:48:11 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace st10303347_PROG6221POE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
